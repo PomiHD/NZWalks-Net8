e8f52b1e56346e39ad3dc1a6de1baeb3e9f0b092
PomiHD <wsgddjy@live.com>
2024-01-28 01:33:24 +1000

    add students api

---
+++ NZWalks/Controllers/StudentsController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.Controllers
+{
+    // https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentsNames = new string[] { "John", "Jane", "Mark", "Emily", "David" };
+            return Ok(studentsNames);
+        }
+    }
+}
