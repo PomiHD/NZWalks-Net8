82e0bcb36459a3fe5edf7ddd78a475fa1f21dd6c
PomiHD <wsgddjy@live.com>
2024-01-29 02:11:09 +1000

    26. Get Region By Id Action Method

--- NZWalks/Controllers/RegionsController.cs
+++ NZWalks/Controllers/RegionsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.Data;
 using NZWalks.Models.Domain;
 
 namespace NZWalks.Controllers
@@ -13,28 +14,18 @@ namespace NZWalks.Controllers
     [ApiController]
     public class RegionsController : ControllerBase
     {
+        private readonly NZWalksDbContext dbContext;
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
         //Get all regions
         // Get: https://localhost:7103/api/regions
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions = new List<Region>
-            {
-                new Region()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Auckland Region",
-                    Code = "AKL",
-                    RegionImageUrl = "blob:https://photos.onedrive.com/05b0d0e3-b31c-4892-ab1d-05ac53f89b32"
-                },
-                new Region()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Wellington  Region",
-                    Code = "WLG",
-                    RegionImageUrl = "blob:https://photos.onedrive.com/05b0d0e3-b31c-4892-ab1d-05ac53f89b32"
-                }
-            };
+            var regions = dbContext.Regions.ToList();
+           
             return Ok(regions);
         }
     }
