2157cf99bbf9c3c68241c9b5fd2136acf264fbc8
PomiHD <wsgddjy@live.com>
2024-01-29 04:24:43 +1000

    28. Change Methods To Use DTOs

--- NZWalks/Controllers/RegionsController.cs
+++ NZWalks/Controllers/RegionsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.Data;
 using NZWalks.Models.Domain;
+using NZWalks.Models.DTO;
 
 namespace NZWalks.Controllers
 {
@@ -15,18 +16,45 @@ namespace NZWalks.Controllers
     public class RegionsController : ControllerBase
     {
         private readonly NZWalksDbContext dbContext;
+
         public RegionsController(NZWalksDbContext dbContext)
         {
             this.dbContext = dbContext;
         }
+
         //Get all regions
         // Get: https://localhost:7103/api/regions
         [HttpGet]
         public IActionResult GetAll()
         {
             var regions = dbContext.Regions.ToList();
-           
+
             return Ok(regions);
+        }
+
+        //Get region by Id
+        // Get: https://localhost:7103/api/regions/{id}
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            // var regions = dbContext.Regions.Find(id);
+            // Get Region Domain Model From Database
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            //Map Region Domain Model to Region Dto
+            var regionDto = new RegionDto
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = null
+            };
+            return Ok(regionDto);
         }
     }
 }
