9495d6bca23e4f4959b9008eaace477d54246b1e
PomiHD <wsgddjy@live.com>
2024-02-08 16:59:03 +1000

    40. Seeding Data Using Entity Framework Core

--- NZWalks/Migrations/20240208065218_Seeding Difficulties and Regions to Database.Designer.cs
+++ NZWalks/Migrations/20240208065218_Seeding Difficulties and Regions to Database.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NZWalks.Data;
 
@@ -11,9 +12,11 @@ using NZWalks.Data;
 namespace NZWalks.Migrations
 {
     [DbContext(typeof(NzWalksDbContext))]
-    partial class NZWalksDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240208065218_Seeding Difficulties and Regions to Database")]
+    partial class SeedingDifficultiesandRegionstoDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
