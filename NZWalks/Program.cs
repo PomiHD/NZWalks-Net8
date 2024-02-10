9e1fdc22d5f7da7add96eedb5b10a365aec77cef
PomiHD <wsgddjy@live.com>
2024-02-10 21:22:50 +1000

    57. Setting Up Authentication - Install Nuget Packages

--- NZWalks/Program.cs
+++ NZWalks/Program.cs
@@ -1,4 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using NZWalks.Data;
 using NZWalks.Mappings;
 using NZWalks.Models.Repositories;
@@ -11,11 +14,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//add db context
 builder.Services.AddDbContext<NzWalksDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
+//add difficulty repository
 builder.Services.AddScoped<IRegionRepository, SqlRegionRepository>();
+//add walk repository
 builder.Services.AddScoped<IWalkRepository, SqlWalkRepository>();
+//add automapper
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+//add authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,7 +45,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+//add authentication middleware to the pipeline
+app.UseAuthentication();
+//add authorization middleware to the pipeline
 app.UseAuthorization();
 
 app.MapControllers();
