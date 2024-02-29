75991031cdbf42c2d9cc10fe336f11deee45ec00
Samuel Yin <wsgddjy@live.com>
2024-03-01 02:01:48 +1000

    71. DbContext changes and Migrations For Image Upload

--- NZWalks/Migrations/20240229160022_Adding Images tabel.Designer.cs
+++ NZWalks/Migrations/20240229160022_Adding Images tabel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NZWalks.Data;
 
@@ -11,9 +12,11 @@ using NZWalks.Data;
 namespace NZWalks.Migrations
 {
     [DbContext(typeof(NzWalksDbContext))]
-    partial class NZWalksDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240229160022_Adding Images tabel")]
+    partial class AddingImagestabel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
